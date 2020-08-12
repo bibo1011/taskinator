dd1ea2c4d9b213a4f78058d1e0a82e7631908106
Blue Abad <bibo1011@gmail.com>
2020-08-12 16:10:20 -0700

    start submit button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,23 @@
-var buttonEl = document.querySelector("#save-task");
-var tasksToDoEl = document.querySelector("#tasks-to-do");
-//console.log(buttonEl);
+var formEl = document.querySelector("#task-form");
 
-buttonEl.addEventListener("click", function(){
+
+//buttonEl.addEventListener("click", function(){
+ formEl.addEventListener("submit", createTaskHandler);
+/*
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
   });
-
-var createTaskHandler = function() {
+*/
+  var createTaskHandler = function(event) {
+    var tasksToDoEl = document.querySelector("#tasks-to-do");
+    event.preventDefault();
+  
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-  }
+    console.log(tasksToDoEl);
+  };
+
